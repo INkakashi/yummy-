c103ca386950e5607f167b1b85f0512662879b22
INkakashi <deewakarsharma687@gmail.com>
2025-10-05 21:37:25 +0530

    added upload

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const passport = require('passport');
 var router = express.Router();
 var userModel = require("./users")
 const upload = require("./multer")
-
-const userModel = require("./users");
 const recipeModel = require("./recipe");
-const commentModel = require("./comments");
+const commentModel = require("./comment");
 
 const localStrategy = require("passport-local");
 
@@ -21,9 +19,14 @@ router.get('/register',function(req,res,next){
   res.render('register')
 })
 
-router.get('/', isLoggedIn, function(req, res, next) {
-  res.render('home');
+router.get('/', isLoggedIn,async function(req, res, next) {
+  recipe = await recipeModel.find()
+  res.render('home', { recipe: recipe });
 });
+
+router.get('/upload', function(req, res, next) {
+  res.render('upload');
+})
 
 router.get('/logout', function(req,res, next){
   req.logout(function(err){
@@ -45,17 +48,19 @@ router.post('/register',function(req,res){
 })
 
 router.post('/upload', upload.single('image'),async function(req, res) {
-    const user = await userModel.findOne({username: req.session.passport.user});
-    const recipe = new recipeModel({
-        title: req.body.title,
+  username = req.session.passport.user;
+    const user = await userModel.findOne({username: username});
+    const recipe = await new recipeModel({
+        name: req.body.title,
         image: req.file.filename,
         ingredients: req.body.ingredients,
-        steps: req.body.steps,
+        instructions: req.body.steps,
         user: user._id
     });
-    user.recipes.push(recipe.id);
+    await user.recipes.push(recipe.id);
     await user.save();
     await recipe.save();
+    res.redirect('/')
   });
 
 router.post('/login', passport.authenticate('local',{
